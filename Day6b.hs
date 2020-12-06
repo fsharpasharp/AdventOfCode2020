eac0b012ff16d176c3119fbb138c77038cf85892
Jan <jnliuu@gmail.com>
2020-12-06 13:06:18 +0100

    Use lines.

--- Day6b.hs
+++ Day6b.hs
@@ -10,12 +10,12 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
 
-solve :: FilePath -> IO [Int]
+solve :: FilePath -> IO Int
 solve file = do
   f <- T.readFile file
   return .  solve' $ f
 
-solve' :: Text -> [Int]
-solve' = fmap (length . foldr1 intersect . fmap T.unpack . T.splitOn "\n") . T.splitOn "\n\n"
+solve' :: Text -> Int
+solve' = sum . fmap (length . foldr1 intersect . fmap T.unpack . T.lines) . T.splitOn "\n\n"
 
 
