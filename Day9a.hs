a2620749b8ac6bb1327f6d25510df6743b9949f8
Jan <jnliuu@gmail.com>
2020-12-09 06:50:25 +0100

    Clean up.

--- Day9a.hs
+++ Day9a.hs
@@ -5,18 +5,16 @@ where
 
 import Data.List (find)
 
-solve :: (Read a, Eq a, Num a) => FilePath -> IO (Maybe ([a], a))
+solve :: (Read b, Eq b, Num b) => FilePath -> IO (Maybe b)
 solve file = do
   lines <- fmap read . lines <$> readFile file
-
-  return $ find (\(x, y) -> y `notElem` sums x) . groupLines $ lines
+  return $ snd <$> (find (\(x, y) -> y `notElem` sums x) . groupLines $ lines)
 
 groupLines :: [b] -> [([b], b)]
 groupLines [] = []
-groupLines lines@(_ : xs) = (preamble, y) : groupLines xs
+groupLines lines@(_ : xs) = (preamble, lines !! 26) : groupLines xs
   where
     preamble = take 25 lines
-    (y : _) = drop 25 lines
 
 sums :: Num a => [a] -> [a]
 sums [] = []
